ef50a91c9909869ffd04ef77b371f1e8e4693ac9
Sathyakrish097 <sathyakrishaji43.gmail.com>
2025-07-26 16:36:20 +0530

    Added test.py file

---
+++ test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+
+def main():
+    start_time= datetime.time()
+    
+    df_test= pd.read_csv(r'D:\Power BI\Utube-tute\Hospitality-Project\fact_aggregated_bookings.csv')
+    print(df_test.columns)
+    print(df_test.shape)
+    print("File Loaded")
+    
+    print("Script Completed")
+    
+    end_time= datetime.time()
+    
+    total= (end_time-start_time)/60
+    print("Total time ", total)
+
+if __name__ == "__main__":
+    main()
